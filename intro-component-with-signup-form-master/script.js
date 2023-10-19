e7110108d5bb7283f452b9429e6587edca1f3996
Mayara Assis <mayaraassisn@gmail.com>
2023-10-19 17:42:30 -0300

    adicionando js

---
+++ intro-component-with-signup-form-master/script.js
@@ -0,0 +1,30 @@
+const name = document.getElementById('name')
+const mail = document.getElementById('mail')
+const last = document.getElementById('lastname')
+const password = document.getElementById('password')
+const btn = document.getElementById('btn')
+
+const error_name = document.getElementsByClassName('error_name');
+const error_last = document.getElementsByClassName('error_last');
+const error_mail = document.getElementsByClassName('error_mail');
+const error_password = document.getElementsByClassName('error_password');
+
+btn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    if(name.value == ''){
+        error_name[0].style.display = 'block'
+        error_name[1].style.display = 'block'
+    }
+    if(last.value == ''){
+        error_last[0].style.display = 'block'
+        error_last[1].style.display = 'block'
+    }
+    if(mail.value == ''){
+        error_mail[0].style.display = 'block'
+        error_mail[1].style.display = 'block'
+    }
+    if(password.value == ''){
+        error_password[0].style.display = 'block'
+        error_password[1].style.display = 'block'
+    }
+})
